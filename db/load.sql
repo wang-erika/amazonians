5b8fa0f83a05466ee3457986e16e014ebd95e65f
Havish Malladi <35409301+hsmalladi@users.noreply.github.com>
2022-10-13 18:22:18 -0400

    load

--- db/load.sql
+++ db/load.sql
@@ -9,8 +9,37 @@ SELECT pg_catalog.setval('public.users_id_seq',
 SELECT pg_catalog.setval('public.products_id_seq',
                          (SELECT MAX(id)+1 FROM Products),
                          false);
+\COPY Inventory FROM 'Inventory.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(pid)+1 FROM Inventory),
+                         false);
 
 \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
 SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
+                         (SELECT MAX(pid)+1 FROM Purchases),
+                         false);
+
+\COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Sellers),
+                         false);
+
+\COPY Cart FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(pid)+1 FROM Cart),
+                         false);
+
+\COPY RatesSeller FROM 'RatesSeller.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(uid)+1 FROM RatesSeller),
+                         false);
+
+\COPY RatesProduct FROM 'RatesProduct.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(pid)+1 FROM RatesProduct),
+                         false);
+
+\COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(orid)+1 FROM Sellers),
                          false);
