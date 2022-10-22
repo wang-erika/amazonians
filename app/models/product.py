645fb9b5b8c9bd154ab482ca34b7f1a0f182f5b7
Eric Xing <esx2@duke.edu>
2022-10-22 14:31:47 -0400

    working on products issue

--- app/models/product.py
+++ app/models/product.py
@@ -2,12 +2,14 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, id, name, price, available):
+    def __init__(self, id, image, category, name, unit_price, description):
         self.id = id
+        self.image = image
+        self.category = category
         self.name = name
-        self.price = price
-        self.available = available
-
+        self.unit_price = unit_price
+        self.description = description
+    """
     @staticmethod
     def get(id):
         rows = app.db.execute('''
@@ -17,13 +19,13 @@ WHERE id = :id
 ''',
                               id=id)
         return Product(*(rows[0])) if rows is not None else None
-
+    """
     @staticmethod
     def get_all(available=True):
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE available = :available
+SELECT id, name, unit_price
+FROM Products, Inventory
+WHERE Inventory.pid = Products.id AND Inventory.quantity > 0
 ''',
                               available=available)
         return [Product(*row) for row in rows]
