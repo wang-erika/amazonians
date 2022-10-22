345de2aad318f7af65bd6c32cae763c0a76fce9f
Bob Qian <bsq2@duke.edu>
2022-10-22 14:55:30 -0400

    seller endpoint works

--- app/index.py
+++ app/index.py
@@ -4,6 +4,7 @@ import datetime
 
 from .models.product import Product
 from .models.purchase import Purchase
+from .models.inventory import Inventory
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -23,3 +24,15 @@ def index():
     return render_template('index.html',
                            avail_products=products,
                            purchase_history=purchases)
+
+@bp.route('/sell', methods=['GET', 'POST'])
+def inventory_page():
+    if current_user.is_authenticated:
+        # get all products they are selling
+        inventory = Inventory.get_by_sid(current_user.id)
+    else:
+        inventory = None
+
+    # render Sell page (shows inventory)
+    return render_template('inventory.html', 
+                            inventory = inventory)
