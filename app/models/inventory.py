3827f344f07e30e3fbbf064560cd97ee647a87a0
Bob Qian <bsq2@duke.edu>
2022-10-23 20:19:06 -0400

    joined inventory query with products to have product info like name, desc, etc.

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -2,31 +2,35 @@ from flask import current_app as app
 
 
 class Inventory:
-    def __init__(self, sid, pid, quantity):
+    def __init__(self, sid, pid, name, quantity, unit_price, description):
         self.sid = sid
         self.pid = pid
+        self.name = name
         self.quantity = quantity
+        self.unit_price = unit_price
+        self.description = description
+
 
     # get a single inventory tuple
-    @staticmethod
-    def get(sid, pid):
-        rows = app.db.execute('''
-SELECT sid, pid, quantity
-FROM Inventory
-WHERE pid = :pid AND sid = :sid
-''',
-                              pid=pid,
-                              sid=sid)
-        return Inventory(*(rows[0])) if rows else None
+#     @staticmethod
+#     def get(sid, pid):
+#         rows = app.db.execute('''
+# SELECT sid, pid, quantity
+# FROM Inventory
+# WHERE pid = :pid AND sid = :sid
+# ''',
+#                               pid=pid,
+#                               sid=sid)
+#         return Inventory(*(rows[0])) if rows else None
 
-    # TODO should prob join with Products to get product info once Havish finishes that
-    # given id of seller, return list of products in their inventory (and quantity)
+    # given id of seller, return list of products in their inventory (and associated info)
     @staticmethod
     def get_by_sid(sid):
         rows = app.db.execute('''
-SELECT sid, pid, quantity
-FROM Inventory
-WHERE sid = :sid
+select sid, pid, name, quantity, unit_price, description
+from Inventory join Products
+    on Inventory.pid = Products.id
+where sid = :sid
 ''',
                               sid=sid)
         return [Inventory(*row) for row in rows]
