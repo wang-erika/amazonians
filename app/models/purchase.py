8c6f2f599d068d180285d3984c4e0aa92c3cc0bf
Bob Qian <bsq2@duke.edu>
2022-11-08 21:00:46 -0500

    updated purchase backend to new db schema

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,18 +2,22 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased, name, price):
+    def __init__(self, id, oid, uid, pid, product_name, quantity, time_purchased, unit_price_at_time_of_payment, fulfilled):
         self.id = id
+        self.oid = oid
         self.uid = uid
         self.pid = pid
+        self.product_name = product_name
+        self.quantity = quantity
         self.time_purchased = time_purchased
-        self.name = name
-        self.price = price
+        self.unit_price_at_time_of_payment = unit_price_at_time_of_payment
+        self.fulfilled = fulfilled
+
 
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT Purchases.id, Purchases.uid, Purchases.pid, time_purchased, Products.name, Products.unit_price,
+SELECT Purchases.id, Purchases.oid, Purchases.uid, Purchases.pid, Products.name as product_name, time_purchased, unit_price_at_time_of_payment, fulfilled
 FROM Purchases, Products
 WHERE Purchases.id = :id, and Purchases.pid = Products.id
 ''',
@@ -23,7 +27,7 @@ WHERE Purchases.id = :id, and Purchases.pid = Products.id
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
-SELECT Purchases.id, Purchases.uid, Purchases.pid, Purchases.time_purchased, Products.name, Products.unit_price
+SELECT Purchases.id, Purchases.oid, Purchases.uid, Purchases.pid, Products.name as product_name, time_purchased, unit_price_at_time_of_payment, fulfilled
 FROM Purchases, Products
 WHERE uid = :uid and Purchases.pid = Products.id
 AND time_purchased >= :since
@@ -33,11 +37,14 @@ ORDER BY time_purchased DESC
                               since=since)
         return [Purchase(*row) for row in rows]
 
+
     @staticmethod
     def get_purchases(uid):
         rows = app.db.execute('''
-SELECT Purchases.id, uid, pid, time_purchased, name, unit_price
+SELECT Purchases.id, Purchases.oid, Purchases.uid, Purchases.pid, Products.name as product_name, time_purchased, unit_price_at_time_of_payment, fulfilled
 FROM Purchases, Products
 WHERE uid = :uid and Purchases.pid = Products.id
-''',    uid = uid)
+''',    
+                              uid = uid)
+        
         return [Purchase(*row) for row in rows]
