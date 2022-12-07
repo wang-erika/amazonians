3bbd43d8a186fb4b59e4aee5ec661da3cb12baea
Havish Malladi <hsm13@duke.edu>
2022-12-07 17:48:33 -0500

    more comments

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -6,6 +6,8 @@ import datetime
 # this file is used for generating the data for the database
 
 # the number of each category we want in our tables
+# only need to change these numbers and can rerun with all the data
+# we want
 num_users = 100
 num_products = 2000
 num_purchases = 2500
@@ -77,18 +79,22 @@ def gen_sellers():
         for id in range(100):
             if id % 100 == 0:
                 print(f'{id}', end=' ', flush=True)
+            # guaranteeing a seller is also a user
             sid = fake.random_int(max=num_users-1)
             while sid in sellers:
                 sid = fake.random_int(max=num_users-1)
             sellers.add(sid)
+            # write to the csv
             writer.writerow([sid])
     return sellers
 
+# getting inventory products
 def gen_inventory(sellers, products):
     with open('./db/data/Inventory.csv', 'w') as f:
         writer = get_csv_writer(f)
         used_keys = set()
         print('Inventory...', end=' ', flush=True)
+        # making every product avaliable for now
         for pid in range(inventory):
             if pid % 100 == 0:
                 print(f'{pid}', end=' ', flush=True)
@@ -100,13 +106,14 @@ def gen_inventory(sellers, products):
         print(f'{inventory} generated')
     return
 
+# creating the items in others carts
 def gen_cart(users, products, sellers):
     combos = set()
     with open('./db/data/Cart.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Cart...', end=' ', flush=True)
         for id in range(cart):
-            # Update to select from sellers
+            # Guaranteeing we have unqiue uid, pid combos and these numbers come from exisiting numbers
             uid = fake.random_element(elements=list(users))
             pid = fake.random_element(elements=list(products))
             while (uid, pid) in combos:
@@ -115,30 +122,36 @@ def gen_cart(users, products, sellers):
             sid = fake.random_element(elements=list(sellers))
             combos.add((uid, pid))
             quantity = fake.random_int(max=20)
+            # write to the csv
             writer.writerow([uid, pid, sid, quantity])
 
-
+# method to generate both product and seller reviews
 def gen_rates(filename, people, users):
     combo = set()
     with open(filename, 'w') as f:
         writer = get_csv_writer(f)
         print(filename, end=' ', flush=True)
         for num in range(reviews):
+            # ensuring the fid and uid combos are unqiue based on database design
             fid = fake.random_element(elements=people)
             uid = fake.random_element(elements=users)
             while (fid, uid) in combo:
                 fid = fake.random_element(elements=people)
                 uid = fake.random_element(elements=users)
+            # generating rating info 
             date = fake.date_time_this_month().strftime("%Y-%m-%d %H:%M:%S")
             rating = fake.random_int(min=1, max=5)
             review = fake.sentence(nb_words=40)[:-1]
+            # writing to file
             writer.writerow([uid, fid, rating, review, date])
 
+# generating the orders
 def gen_orders(users):
     orders = set()
     with open("./db/data/Orders.csv", 'w') as f:
         writer = get_csv_writer(f)
         print("Orders...", end=' ', flush=True)
+        # making sure oid is unqiue
         for oid in range(order):
             orders.add(oid)
             # Update sid to select from sellers
@@ -149,21 +162,25 @@ def gen_orders(users):
             writer.writerow([oid, uid, date_ordered, fufilled])
     return orders
 
+# generating the purchases
 def gen_purchases(orders, users, products):
     with open('./db/data/Purchases.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Purchases...', end=' ', flush=True)
         oiddict = {}
+        # confirming some purchases belong in the same order
         for id in range(num_purchases):
             if id % 100 == 0:
                 print(f'{id}', end=' ', flush=True)
             
+            # if the user already was involved in another purchase in this order, add accordingly
             oid = fake.random_element(elements=list(orders))
             if oid in oiddict:
                 uid = oiddict[oid]
             else:
                 uid = fake.random_element(elements=list(users))
                 oiddict[oid] = uid
+            # more purchase information
             pid = fake.random_element(elements=list(products))
             quantity = fake.random_int(min=0, max = 20)
             time_purchased = fake.date_time_this_month().strftime("%Y-%m-%d %H:%M:%S")
@@ -174,7 +191,7 @@ def gen_purchases(orders, users, products):
 
 
 
-
+# run all the generation scripts
 users = gen_users()
 products = gen_products()
 sellers = gen_sellers()
